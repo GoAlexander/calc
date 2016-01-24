022bc3802b13803b916842877f5cf362f04c6df1
AlexanderSergeev <asergeev1997@yandex.ru>
2016-01-24 15:06:00 +0300

    now it works!

--- src/MainPoland.java
+++ src/MainPoland.java
@@ -4,15 +4,17 @@ public class MainPoland extends Main {
 
 	public static void main(String[] args) {
 		String str;
+		@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
-		
+
 		// TODO make this menu similar as in Main.java
-		while (true) //Бесконечный цикл
-        {
+		while (true) // Бесконечный цикл
+		{
 			System.out.print(">");
 			str = in.nextLine(); // for all line (with spaces)
-            System.out.println(Poland.Calculate(str)); //Считываем, и выводим результат
-        }
+			Poland.Calculate(str).print();
+			// Считываем, и выводим результат
+		}
 	}
 
 }
--- src/PolandTest.java
+++ src/PolandTest.java
@@ -1,22 +1,16 @@
 import static org.junit.Assert.*;
 
-import org.junit.Ignore;
+//import org.junit.Ignore;
 //import org.junit.Before;
 import org.junit.Test;
 
 public class PolandTest {
-
-	/*@Before
-	public void setUp() {
-		Fraction f1 = new Fraction(0);
-	}*/
 	
-	/*
 	@Test
 	public void test1() {
 		Fraction f1 = new Fraction(8);
 		try {
-			Fraction f2 = Poland.eval("3 + 5");
+			Fraction f2 = Poland.Calculate("3 + 5");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -27,7 +21,7 @@ public class PolandTest {
 	public void test1_2() {
 		Fraction f1 = new Fraction(-1);
 		try {
-			Fraction f2 = Poland.eval("1 - 2");
+			Fraction f2 = Poland.Calculate("1 - 2");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -38,7 +32,7 @@ public class PolandTest {
 	public void test2() {
 		Fraction f1 = new Fraction(25);
 		try {
-			Fraction f2 = Poland.eval("10 + 15");
+			Fraction f2 = Poland.Calculate("10 + 15");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -49,7 +43,7 @@ public class PolandTest {
 	public void test2_2() {
 		Fraction f1 = new Fraction(120);
 		try {
-			Fraction f2 = Poland.eval("120 +0");
+			Fraction f2 = Poland.Calculate("120 +0");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -60,7 +54,7 @@ public class PolandTest {
 	public void test3() {
 		Fraction f1 = new Fraction(1111);
 		try {
-			Fraction f2 = Poland.eval("1 + 10 + 100 + 1000");
+			Fraction f2 = Poland.Calculate("1 + 10 + 100 + 1000");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -71,7 +65,7 @@ public class PolandTest {
 	public void test3_2() {
 		Fraction f1 = new Fraction(9);
 		try {
-			Fraction f2 = Poland.eval("2 + 2 + 5");
+			Fraction f2 = Poland.Calculate("2 + 2 + 5");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -82,7 +76,7 @@ public class PolandTest {
 	public void test4() {
 		Fraction f1 = new Fraction(1111);
 		try {
-			Fraction f2 = Poland.eval("1 + 10 +100+1000");
+			Fraction f2 = Poland.Calculate("1 + 10 +100+1000");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -93,7 +87,7 @@ public class PolandTest {
 	public void test4_2() {
 		Fraction f1 = new Fraction(109);
 		try {
-			Fraction f2 = Poland.eval("3 + 5 + 0 + 1 + 100");
+			Fraction f2 = Poland.Calculate("3 + 5 + 0 + 1 + 100");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -104,7 +98,7 @@ public class PolandTest {
 	public void test5() {
 		Fraction f1 = new Fraction(1);
 		try {
-			Fraction f2 = Poland.eval("1");
+			Fraction f2 = Poland.Calculate("1");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -115,7 +109,7 @@ public class PolandTest {
 	public void test6() {
 		Fraction f1 = new Fraction(311, 2);
 		try {
-			Fraction f2 = Poland.eval("3 + 50 + 1 + 1 + 100 + 1/2");
+			Fraction f2 = Poland.Calculate("3 + 50 + 1 + 1 + 100 + 1/2");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -126,7 +120,7 @@ public class PolandTest {
 	public void test7() {
 		Fraction f1 = new Fraction(1);
 		try {
-			Fraction f2 = Poland.eval("1/2 + 1/2");
+			Fraction f2 = Poland.Calculate("1/2 + 1/2");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -146,7 +140,7 @@ public class PolandTest {
 	public void test7_2() {
 		Fraction f1 = new Fraction(6, 4);
 		try {
-			Fraction f2 = Poland.eval("3/4 + 3/4");
+			Fraction f2 = Poland.Calculate("3/4 + 3/4");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -157,7 +151,7 @@ public class PolandTest {
 	public void test7_3() {
 		Fraction f1 = new Fraction(1);
 		try {
-			Fraction f2 = Poland.eval("3/4 + 1/4");
+			Fraction f2 = Poland.Calculate("3/4 + 1/4");
 			assertTrue(f1.equals(f2));
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
@@ -169,7 +163,7 @@ public class PolandTest {
 	public void test7_4() {
 		Fraction f1 = new Fraction(7, 5);
 		try {
-			Fraction f2 = Poland.eval("5/10 + 7/10 + 2/10");
+			Fraction f2 = Poland.Calculate("5/10 + 7/10 + 2/10");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -180,7 +174,7 @@ public class PolandTest {
 	public void test7_5() {
 		Fraction f1 = new Fraction(6, 5);
 		try {
-			Fraction f2 = Poland.eval("5/10 + 7/10");
+			Fraction f2 = Poland.Calculate("5/10 + 7/10");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -191,7 +185,7 @@ public class PolandTest {
 	public void test7_6() {
 		Fraction f1 = new Fraction(6, 5);
 		try {
-			Fraction f2 = Poland.eval("5/10 + 7/10 + 2/10 - 2/10");
+			Fraction f2 = Poland.Calculate("5/10 + 7/10 + 2/10 - 2/10");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -202,7 +196,7 @@ public class PolandTest {
 	public void test8() {
 		Fraction f1 = new Fraction(5, 4);
 		try {
-			Fraction f2 = Poland.eval("1/2 + 3/4");
+			Fraction f2 = Poland.Calculate("1/2 + 3/4");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -213,7 +207,7 @@ public class PolandTest {
 	public void test9() {
 		Fraction f1 = new Fraction(0);
 		try {
-			Fraction f2 = Poland.eval("-1 + 1");
+			Fraction f2 = Poland.Calculate("-1 + 1");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -222,23 +216,23 @@ public class PolandTest {
 	
 	//next milestone:
 	
-	@Ignore @Test //TODO BUG!!!
+	@Test 
 	
 	public void test10() {
 		Fraction f1 = new Fraction(10);
 		try {
-			Fraction f2 = Poland.eval("5* 2");
+			Fraction f2 = Poland.Calculate("5* 2");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
 		}
 	}
 	
-	@Ignore @Test //TODO BUG!!!
+	@Test //TODO BUG!!!
 	public void test11() {
 		Fraction f1 = new Fraction(12);
 		try {
-			Fraction f2 = Poland.eval("2 + 5* 2");
+			Fraction f2 = Poland.Calculate("2 + 5* 2");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
@@ -249,18 +243,18 @@ public class PolandTest {
 		Fraction f1 = new Fraction(7 / 32);
 		Fraction f2;
 		try {
-			f2 = Poland.eval("1/32-2/32+1/4");
+			f2 = Poland.Calculate("1/32-2/32+1/4");
 			assertTrue(f1.equals(f2));
 		} catch (Exception e) {
 			fail();
 		}
 	}
 
-	@Ignore @Test
+	@Test
 	public void test13() {
 		try {
 			@SuppressWarnings("unused")
-			Fraction f = Poland.eval("a+35");
+			Fraction f = Poland.Calculate("a+35");
 			fail();
 		} catch (Exception e) {
 			assertTrue(true);
@@ -271,7 +265,7 @@ public class PolandTest {
 	public void test14_1() {
 		try {
 			@SuppressWarnings("unused")
-			Fraction f2 = Poland.eval("+ - 1");
+			Fraction f2 = Poland.Calculate("+ - 1");
 			fail();
 		} catch (Exception e) {
 			assertTrue(true);
@@ -281,11 +275,11 @@ public class PolandTest {
 	@Test
 	public void test14_2() {
 		try {
-			Fraction f1 = new Fraction(0);
-			Fraction f2 = Poland.eval("1+ - 1");
-			assertTrue(f1.equals(f2));
-		} catch (Exception e) {
+			@SuppressWarnings("unused")
+			Fraction f2 = Poland.Calculate("1+ - 1");
 			fail();
+		} catch (Exception e) {
+			assertTrue(true);
 		}
 	}
 
@@ -293,7 +287,7 @@ public class PolandTest {
 	public void test15() {
 		try {
 			@SuppressWarnings("unused")
-			Fraction f2 = Poland.eval("+");
+			Fraction f2 = Poland.Calculate("+");
 			fail();
 		} catch (Exception e) {
 			assertTrue(true);
@@ -304,12 +298,32 @@ public class PolandTest {
 	public void test16() {
 		try {
 			@SuppressWarnings("unused")
-			Fraction f2 = Poland.eval("1 + -");
+			Fraction f2 = Poland.Calculate("1 + -");
 			fail();
 		} catch (Exception e) {
 			assertTrue(true);
 		}
 	}
-	*/
 	
+	@Test
+	public void test17() {
+		Fraction f1 = new Fraction(4,5);
+		try {
+			Fraction f2 = Poland.Calculate("2/ (5/2)");
+			assertTrue(f1.equals(f2));
+		} catch (Exception e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void test18() {
+		Fraction f1 = new Fraction(9,2);
+		try {
+			Fraction f2 = Poland.Calculate("2/1 + 5/(2/1)");
+			assertTrue(f1.equals(f2));
+		} catch (Exception e) {
+			fail();
+		}
+	}	
 }
