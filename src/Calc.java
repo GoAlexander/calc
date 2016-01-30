73351fe79ed7738e0c11de76d2399e9c36964c4f
AlexanderSergeev <asergeev1997@yandex.ru>
2016-01-30 21:04:22 +0300

    Priority on the way

--- src/Calc.java
+++ src/Calc.java
@@ -1,20 +1,32 @@
 
 class Calc {
-	
-	static boolean debug = true; //for DEBUG
+
+	static boolean debug = false; // for DEBUG
 
 	// Expression calculation
 	public static Fraction calculate(String s) throws Exception {
 		if (debug)
 			System.out.println("DEBUG: " + s);
-		
+
 		String str = parse(s);
-		
+		// Substitute expression in brackets with result
+		String tmp;
+		while (str.contains("(")) {
+			tmp = str.substring(str.lastIndexOf("(") + 1, str.indexOf(")", str.lastIndexOf("(") + 1));
+			tmp = calc(tmp).toString();
+			str = str.replace(str.substring(str.lastIndexOf("("), str.indexOf(")", str.lastIndexOf("(") + 1) + 1), tmp);
+		}
 		/*
 		 * if (str.startsWith(" +") || str.startsWith(" -") || str.startsWith(
 		 * " *") || str.startsWith(" %")) throw new Exception();
 		 */
 		return calc(str);
+	}
+
+	static private boolean isOperator(char c) {
+		if (("+-/*".indexOf(c) != -1))
+			return true;
+		return false;
 	}
 
 	// Parsing
@@ -33,24 +45,58 @@ class Calc {
 		}
 		if (str.contains("*")) {
 			str = str.replace("*", " * ");
+
 		}
 		if (str.contains("%")) {
 			str = str.replace("%", " % ");
 		}
-		
+
+		int first = -2, last = -2;
+		char arr[] = str.toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == '*' || arr[i] == '%') {
+				for (int j = i - 1; j != 0; j--) {
+					if (arr[j] == '(') {
+						first = -2;
+						break;
+					}
+					if (isOperator(arr[j])) {
+						first = j;
+						break;
+					}
+					first = j;
+				}
+				for (int j = i + 1; j != arr.length; j++) {
+					if (arr[j] == ')') {
+						last = -2;
+						break;
+					}
+					if (isOperator(arr[j])) {
+						last = j;
+						break;
+					}
+					last = j;
+				}
+				if (first != -2 && last != -2) {
+					String tmp;
+					if (last == arr.length - 1)
+						last = last + 1;
+					else
+						last = last - 1;
+					if (first != 1)
+						first = first + 2;
+					else
+						first = first - 1;
+					tmp = str.substring(first, last);
+					str = str.replace(tmp, "(" + tmp + ")");
+				}
+				System.out.println("DEBUG: " + str);
+			}
+		}
+
 		if (debug)
 			System.out.println("DEBUG: " + str);
 
-		// Substitute expression in brackets with result
-		String tmp;
-		while (str.contains("(")) {
-			tmp = str.substring(str.lastIndexOf("(") + 1, str.indexOf(")", str.lastIndexOf("(") + 1));
-			tmp = calc(tmp).toString();
-			str = str.replace(str.substring(str.lastIndexOf("("), str.indexOf(")", str.lastIndexOf("(") + 1) + 1), tmp);
-			
-			if (debug)
-				System.out.println("DEBUG: " + str);
-		}
 		return str;
 	}
 
