5f88141bc0d71bd86a1e674d6382e70503d124df
AlexanderSergeev <asergeev1997@yandex.ru>
2016-02-01 10:52:55 +0300

    Commit

---
+++ src/fraction/FractionTest.java
@@ -0,0 +1,38 @@
+package fraction;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FractionTest {
+
+	@Test
+	public void test1() {
+		Fraction f1 = new Fraction(1,2);
+		Fraction f2 = Fraction.sum(new Fraction(1,4), new Fraction(1,4));
+		assertTrue(f1.equals(f2));
+	}
+	
+	@Test
+	public void test2() {
+		Fraction f1 = new Fraction(1,2);
+		Fraction f2 = Fraction.dif(new Fraction(3,4), new Fraction(1,4));
+		assertTrue(f1.equals(f2));
+	}
+	
+	@Test
+	public void test3() {
+		Fraction f1 = new Fraction(1,2);
+		Fraction f2 = Fraction.mult(new Fraction(1), new Fraction(1,2));
+		assertTrue(f1.equals(f2));
+	}
+	
+	@Test
+	public void test4() {
+		Fraction f1 = new Fraction(1,2);
+		Fraction f2 = Fraction.div(new Fraction(1,4), new Fraction(1,2));
+		assertTrue(f1.equals(f2));
+	}
+	
+
+}
