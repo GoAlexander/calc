3e7a7ce41283a2cbf384f80da24957bc57285193
GoAlexander <AlexanderDydychkin@yandex.ru>
2016-02-18 00:26:04 +0300

    all work instead of undo/redo andhistory

--- src/gui/BigGUI.java
+++ src/gui/BigGUI.java
@@ -8,44 +8,22 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
-import javax.swing.JSplitPane;
 import javax.swing.JPanel;
 import java.awt.event.ActionListener;
 import java.util.EmptyStackException;
 import java.awt.event.ActionEvent;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
 
 import exception.IllegalExpression;
 import fraction.Fraction;
 import poland.Poland;
 
 import java.awt.GridLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.SpringLayout;
-import javax.swing.BoxLayout;
-import javax.swing.JLayeredPane;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.FlowLayout;
-import org.eclipse.wb.swing.FocusTraversalOnArray;
-import java.awt.Component;
-import java.awt.Dimension;
-import javax.swing.JSeparator;
+import javax.swing.JLabel;
 
 public class BigGUI extends GUI {
 
 	private JFrame frmFractionCalculator;
 	private JTextField textField;
-	private JButton btnNewButton;
-	private JButton btnNewButton_1;
-	private JButton btnNewButton_2;
-	private JButton btnNewButton_3;
-	private JButton btnNewButton_4;
 	
 	String str;
 	Fraction result;
@@ -108,84 +86,98 @@ public class BigGUI extends GUI {
 		JButton button_2 = new JButton("/");
 		button_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "/");
 			}
 		});
 		panel.add(button_2);
 		JButton button_3 = new JButton("7");
 		button_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "7");
 			}
 		});
 		panel.add(button_3);
 		JButton button_4 = new JButton("8");
 		button_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "8");
 			}
 		});
 		panel.add(button_4);
 		JButton button_5 = new JButton("9");
 		button_5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "9");
 			}
 		});
 		panel.add(button_5);
 		JButton button_6 = new JButton("*");
 		button_6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "*");
 			}
 		});
 		panel.add(button_6);
 		JButton button_7 = new JButton("4");
 		button_7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "4");
 			}
 		});
 		panel.add(button_7);
 		JButton button_8 = new JButton("5");
 		button_8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "5");
 			}
 		});
 		panel.add(button_8);
 		JButton button_9 = new JButton("6");
 		button_9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "6");
 			}
 		});
 		panel.add(button_9);
 		JButton button_10 = new JButton("-");
 		button_10.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "-");
 			}
 		});
 		panel.add(button_10);
 		JButton button_11 = new JButton("1");
 		button_11.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "1");
 			}
 		});
 		panel.add(button_11);
 		JButton button_12 = new JButton("2");
 		button_12.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "2");
 			}
 		});
 		panel.add(button_12);
 		JButton button_13 = new JButton("3");
 		button_13.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "3");
 			}
 		});
 		panel.add(button_13);
 		JButton button_14 = new JButton("+");
 		button_14.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "+");
 			}
 		});
 		panel.add(button_14);
 		JButton button_15 = new JButton("0");
 		button_15.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "0");
 			}
 		});
 		panel.add(button_15);
@@ -214,9 +206,11 @@ public class BigGUI extends GUI {
 			}
 		});
 		
-		JSeparator separator = new JSeparator();
-		panel.add(separator);
-		panel.add(button);
+		JLabel lblNewLabel = new JLabel("\u263A");
+		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblNewLabel);
+		panel.add(button); //add = button
 		
 		
 		
