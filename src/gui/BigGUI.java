bc746c3cb1aa50e458ff729c35603b4413816b92
AlexanderSergeev <asergeev1997@yandex.ru>
2016-02-18 21:15:58 +0300

    BigGUI new

--- src/gui/BigGUI.java
+++ src/gui/BigGUI.java
@@ -6,8 +6,9 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+
 import javax.swing.JButton;
-import javax.swing.SwingConstants;
 import javax.swing.JPanel;
 import java.awt.event.ActionListener;
 import java.util.EmptyStackException;
@@ -16,15 +17,13 @@ import java.awt.event.ActionEvent;
 import exception.IllegalExpression;
 import fraction.Fraction;
 import poland.Poland;
-
-import java.awt.GridLayout;
-import javax.swing.JLabel;
+import java.awt.GridBagLayout;
 
 public class BigGUI extends GUI {
 
 	private JFrame frmFractionCalculator;
 	private JTextField textField;
-	
+
 	String str;
 	Fraction result;
 
@@ -57,143 +56,285 @@ public class BigGUI extends GUI {
 	private void initialize() {
 		frmFractionCalculator = new JFrame();
 		frmFractionCalculator.setTitle("Fraction Calculator");
-		frmFractionCalculator.setBounds(100, 100, 450, 300);
+		frmFractionCalculator.setBounds(100, 100, 450, 360);
 		frmFractionCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		textField = new JTextField();
 		textField.setFont(new Font("Dialog", Font.BOLD, 30));
 		frmFractionCalculator.getContentPane().add(textField, BorderLayout.NORTH);
 		textField.setColumns(10);
-		
+
 		JPanel panel = new JPanel();
 		frmFractionCalculator.getContentPane().add(panel, BorderLayout.CENTER);
-		//panel.setLayout(new GridLayout(5, 4));
-		
-		GridLayout buttonLayout = new GridLayout(5, 4);
-		
-		panel.setLayout(buttonLayout);
+		JButton button;
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 
-		
-		panel.add(new JButton("Undo"));
-		panel.add(new JButton("Redo"));
-		JButton button_1 = new JButton("C");
-		button_1.addActionListener(new ActionListener() {
+		GridBagLayout gridbag = new GridBagLayout();
+		panel.setLayout(gridbag);
+
+		// natural height, maximum width
+		c.fill = GridBagConstraints.HORIZONTAL;
+
+		button = new JButton("Undo");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.ipady = 20;
+		c.weightx = 1;
+		c.gridx = 0;
+		c.gridy = 0;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("Redo");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 1;
+		c.gridy = 0;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("C");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText("");
 			}
 		});
-		panel.add(button_1);
-		JButton button_2 = new JButton("/");
-		button_2.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 2;
+		c.gridy = 0;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("/");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "/");
 			}
 		});
-		panel.add(button_2);
-		JButton button_3 = new JButton("7");
-		button_3.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 3;
+		c.gridy = 0;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("7");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "7");
 			}
 		});
-		panel.add(button_3);
-		JButton button_4 = new JButton("8");
-		button_4.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("8");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "8");
 			}
 		});
-		panel.add(button_4);
-		JButton button_5 = new JButton("9");
-		button_5.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 1;
+		c.gridy = 1;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("9");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "9");
 			}
 		});
-		panel.add(button_5);
-		JButton button_6 = new JButton("*");
-		button_6.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 2;
+		c.gridy = 1;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("*");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "*");
 			}
 		});
-		panel.add(button_6);
-		JButton button_7 = new JButton("4");
-		button_7.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 3;
+		c.gridy = 1;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("4");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "4");
 			}
 		});
-		panel.add(button_7);
-		JButton button_8 = new JButton("5");
-		button_8.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 0;
+		c.gridy = 2;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("5");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "5");
 			}
 		});
-		panel.add(button_8);
-		JButton button_9 = new JButton("6");
-		button_9.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 1;
+		c.gridy = 2;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("6");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "6");
 			}
 		});
-		panel.add(button_9);
-		JButton button_10 = new JButton("-");
-		button_10.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 2;
+		c.gridy = 2;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("-");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "-");
 			}
 		});
-		panel.add(button_10);
-		JButton button_11 = new JButton("1");
-		button_11.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 3;
+		c.gridy = 2;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("1");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "1");
 			}
 		});
-		panel.add(button_11);
-		JButton button_12 = new JButton("2");
-		button_12.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 0;
+		c.gridy = 3;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("2");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "2");
 			}
 		});
-		panel.add(button_12);
-		JButton button_13 = new JButton("3");
-		button_13.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 1;
+		c.gridy = 3;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("3");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "3");
 			}
 		});
-		panel.add(button_13);
-		JButton button_14 = new JButton("+");
-		button_14.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 2;
+		c.gridy = 3;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("+");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "+");
 			}
 		});
-		panel.add(button_14);
-		JButton button_15 = new JButton("0");
-		button_15.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 3;
+		c.gridy = 3;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("0");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(textField.getText() + "0");
 			}
 		});
-		panel.add(button_15);
-		JButton button_16 = new JButton("history");
-		button_15.addActionListener(new ActionListener() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 0;
+		c.gridy = 4;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("(");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + "(");
 			}
 		});
-		panel.add(button_16);
-		JButton button = new JButton("=");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 1;
+		c.gridy = 4;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton(")");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText() + ")");
+			}
+		});
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 2;
+		c.gridy = 4;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("History");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 3;
+		c.gridy = 4;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
+
+		button = new JButton("=");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				str = textField.getText();
 				try {
-					result = Poland.calculate(str);
-					textField.setText(result.getString());
+					if (!str.equals("")) {
+						result = Poland.calculate(str);
+						textField.setText(result.getString());
+					}
 				} catch (ArithmeticException q) {
 					textField.setText("Error! Do not divide by zero next time!");
 				} catch (IllegalExpression a) {
@@ -205,14 +346,12 @@ public class BigGUI extends GUI {
 				}
 			}
 		});
-		
-		JLabel lblNewLabel = new JLabel("\u263A");
-		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblNewLabel);
-		panel.add(button); //add = button
-		
-		
-		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.gridx = 0;
+		c.gridwidth = 4;
+		c.gridy = 5;
+		gridbag.setConstraints(button, c);
+		panel.add(button);
 	}
 }
