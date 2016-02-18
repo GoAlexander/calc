cefd9a24043caeed5645bc7b35f93607c7e3be70
AlexanderSergeev <asergeev1997@yandex.ru>
2016-02-19 00:08:26 +0300

    GUI Undo Redo

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 
+import emulator.Emulator;
 import exception.*;
 import fraction.Fraction;
 import poland.Poland;
@@ -22,6 +23,7 @@ public class GUI {
 
 	private JFrame frmFractionCalculator;
 
+	Emulator myEmulator = new Emulator();
 	String str;
 	Fraction result;
 	private JTextField textField;
@@ -60,49 +62,58 @@ public class GUI {
 		frmFractionCalculator.setBounds(100, 100, 440, 142);
 		frmFractionCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmFractionCalculator.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+
 		textField = new JTextField();
 		textField.setFont(new Font("Dialog", Font.BOLD, 30));
 		frmFractionCalculator.getContentPane().add(textField, BorderLayout.NORTH);
 		textField.setColumns(10);
-		
+
 		JPanel panel = new JPanel();
 		frmFractionCalculator.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(new GridLayout(2, 2));
-		
+
 		JButton btnNewButton_3 = new JButton("Undo");
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				try {
+					textField.setText(myEmulator.back().toString());
+				} catch (EmptyStackException a) {
+				}
 			}
 		});
 		panel.add(btnNewButton_3);
-		
+
 		JButton btnNewButton = new JButton("Redo");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				try {
+					textField.setText(myEmulator.forward().toString());
+				} catch (EmptyStackException a) {
+				}
 			}
 		});
 		panel.add(btnNewButton);
-		
+
 		JButton btnNewButton_1 = new JButton("History");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				history.setVisible(true);
-				//wait???
-				//textField.setText(history.selected_exp);
+				// wait???
+				// textField.setText(history.selected_exp);
 
 			}
 		});
 		panel.add(btnNewButton_1);
-		
+
 		JButton btnNewButton_2 = new JButton("=");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				str = textField.getText();
 				try {
-					if(!str.equals("")) {
+					if (!str.equals("")) {
 						result = Poland.calculate(str);
 						textField.setText(result.getString());
+						myEmulator.newnumber(str);
 					}
 				} catch (ArithmeticException q) {
 					textField.setText("Error! Do not divide by zero next time!");
